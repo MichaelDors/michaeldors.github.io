b97286c9728a3cfbda13888cf86ea262abc5910b
MichaelDors <115369454+MichaelDors@users.noreply.github.com>
2024-02-09 21:29:48 -0500

    Update script.js
    Signed-off-by: MichaelDors <115369454+MichaelDors@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+if(window.location.href.indexOf("index") > -1){
+    document.getElementById("mobilehome").classList.add("navbuttonfilled");
+}
+if(window.location.href.indexOf("about") > -1){
+    document.getElementById("mobileabout").classList.add("navbuttonfilled");
+}
+if(window.location.href.indexOf("projects") > -1){
+    document.getElementById("mobileprojects").classList.add("navbuttonfilled");
+}
+
 function openmenu() {
     if (document.getElementById("navigation").classList.contains("opennav")){
         document.getElementById("navigation").classList.remove("opennav");
@@ -21,6 +32,17 @@ function openmenu() {
     }
 }
 
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0;
+    setTimeout(function() { 
+        window.location.href = href;
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1;
+})
+
 //window.addEventListener('scroll', e => {
 //    if (window.scrollY > 500) {
 //        document.getElementById("body").classList.add("scrollsnap");
@@ -29,3 +51,91 @@ function openmenu() {
 //        document.getElementById("body").classList.remove("scrollsnap");
 //    }
 //});
+
+
+
+function filters() {
+    var filterBar = document.getElementById("filterbar");
+    var filterTags = document.getElementById("filtertags");
+    var sortTags = document.getElementById("sorttags");
+
+    if(filterbar.classList.contains("openfilterbar") && filterTags.style.display !== "none"){
+        filterBar.classList.remove("openfilterbar");
+        filterTags.style.opacity = "0";
+        sortTags.style.opacity = "0";
+        setTimeout(function () {
+            document.getElementById("sorttags").style.display = "none";
+            document.getElementById("filtertags").style.display = "none";
+        }, 150);
+    }
+    else{
+        document.getElementById("filterbar").classList.add("openfilterbar");
+
+        document.getElementById("sorttags").style.opacity = "0";
+        document.getElementById("filtertags").style.opacity = "1";
+        setTimeout(function () {
+            document.getElementById("filtertags").style.display = "";
+            document.getElementById("sorttags").style.display = "none";
+        }, 200);
+    }
+}
+
+
+
+function sorts(){
+    if(document.getElementById("filterbar").classList.contains("openfilterbar") && document.getElementById("sorttags").style.display !== "none"){
+        document.getElementById("filterbar").classList.remove("openfilterbar");
+
+        document.getElementById("sorttags").style.opacity = "0";
+        document.getElementById("filtertags").style.opacity = "0";
+        setTimeout(function () {
+            document.getElementById("sorttags").style.display = "none";
+            document.getElementById("filtertags").style.display = "none";
+        }, 150);
+    }
+    else{
+        document.getElementById("filterbar").classList.add("openfilterbar");
+
+        document.getElementById("sorttags").style.opacity = "1";
+        document.getElementById("filtertags").style.opacity = "0";
+        setTimeout(function () {
+            document.getElementById("sorttags").style.display = "";
+            document.getElementById("filtertags").style.display = "none";
+        }, 200);
+    }
+}
+
+function filter(type) {
+    document.getElementById("filteranim").opacity = "1";
+    document.getElementById("projectcontainer").style.display = "none";
+    document.getElementById("filterbar").classList.remove("openfilterbar");
+    document.getElementById("sorttags").style.display = "none";
+    document.getElementById("filtertags").style.display = "none";
+    document.getElementById("filteringcontainer").style.display = "";
+    var allElements = document.querySelectorAll('.projectarea');
+
+    if (type.toLowerCase() === "all") {
+        allElements.forEach(function (element) {
+            element.style.display = "";
+        });
+    }
+    else {
+        allElements.forEach(function (element) {
+            if (element.getAttribute('data-projecttype') !== type.toLowerCase()) {
+                element.style.display = "none";
+            }
+            else {
+                element.style.display = "";
+            }
+        });
+    }
+
+
+        AOS.init();
+        setTimeout(function () {
+            document.getElementById("projectcontainer").style.display = "";
+            document.getElementById("filteranim").opacity = "0";
+            document.getElementById("filteringcontainer").style.display = "none";
+        }, 2000);
+        window.scrollTo(0, 0);
+}
