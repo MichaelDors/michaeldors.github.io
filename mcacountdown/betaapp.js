7abf067f3131ec0c44e95a83238e18a288b2e1c6
Michael Dors <115369454+MichaelDors@users.noreply.github.com>
2025-04-07 18:38:58 -0400

    Update betaapp.js

--- mcacountdown/betaapp.js
+++ mcacountdown/betaapp.js
@@ -4331,7 +4331,7 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', eve
     }
 const countdownTitle = document.getElementById("countdowntitle");
 if (countdownTitle) {
-    if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) || parameter("atc") !== "none") {
+    if (((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)) || parameter("atc") !== "none") {
         countdownTitle.style.border = `1px solid rgba(255, 255, 255, 0)`;
         document.documentElement.style.setProperty('--titlergba', 'rgba(0,0,0,0)');
         document.documentElement.style.setProperty('--titleforegroundcolor', 'rgba(255,255,255,1)');
