8c6cbf6b0fc6e6b5f92f60c90fab6f7b26da6ed0
Michael Dors <115369454+MichaelDors@users.noreply.github.com>
2025-04-07 18:47:01 -0400

    Update betaapp.js

--- mcacountdown/betaapp.js
+++ mcacountdown/betaapp.js
@@ -2660,7 +2660,7 @@ if (mlkthisyear - now < 0) {
           const opacity = Math.max(0, 1 - distance / 300) / 3;
   
           // Set element opacity
-          if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) || parameter("atc") !== "none") {
+          if (((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)) || parameter("atc") !== "none") {
             document.getElementById("countdowntitle").style.border = `1px solid rgba(255, 255, 255, ${opacity})`;
             document.documentElement.style.setProperty('--titlergba', 'rgba(0,0,0,0)');
             document.documentElement.style.setProperty('--titleforegroundcolor', 'rgba(255,255,255,1)');
@@ -4302,8 +4302,10 @@ showToast('Pick an exception day to add this event to', 'info')
             document.documentElement.style.setProperty('--scheduleblurbg', 'rgba(239, 239, 239, 0.73)');
             document.documentElement.style.setProperty('--schedulebgbottomblur', '#ffffff');
             document.documentElement.style.setProperty('--progressbarblur', '#ffffffe5');
-            document.documentElement.style.setProperty('--titlergba', 'rgba(0,0,0,0)');
-            document.documentElement.style.setProperty('--titleforegroundcolor', 'rgba(0,0,0,1)');
+            if(parameter("atc")== "none"){
+                document.documentElement.style.setProperty('--titlergba', 'rgba(0,0,0,0)');
+                document.documentElement.style.setProperty('--titleforegroundcolor', 'rgba(0,0,0,1)');
+            }
         }
         
         function setDarkMode() {
